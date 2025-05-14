727a5d379a424957eaa9ac07145d22cb64e54559
lida <l_shafiei_2005@yahoo.com>
2025-05-14 15:22:07 +0330

    merge

--- composables/roles/useRole.service.ts
+++ composables/roles/useRole.service.ts
@@ -12,4 +12,9 @@ export const useGetRolesService = () => {
   const fetchData = useFetchApi();
   return (customConfig: FetchCustomConfig = {}) =>
     fetchData("admin/roles", {}, { setToken: true,toastError: true, ...customConfig });
-};y
+};
+export const useGetRolesAndPermissions = ()=>{
+  const fetchData = useFetchApi<RoleAndPermissions, RoleAndPermissions>(RoleAndPermissions);
+  return (customConfig: FetchCustomConfig = {}) =>
+    fetchData("admin/get-roles-permissions", {}, { setToken: true,toastError: true, ...customConfig });
+}
